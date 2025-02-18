634325fc443feac4fbc56605ee28a6a31006ae07
Andres <andresgayong@gmail.com>
2025-02-17 21:03:18 -0600

    control 3 ptm

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # Cargar variables de entorno
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:MarioKartW3138@localhost:5432/tu_base_de_datos")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:MarioKartW3138@localhost:5432/golf_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
